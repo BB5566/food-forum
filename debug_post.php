7d691c4936dea94cbff62f9e92c643f4af570ff5
BB <mobbsong@gmail.com>
2025-09-15 12:19:05 +0800

    刪除不再使用的 debug_post.php 和 debug_post.txt 檔案，清理專案結構

--- debug_post.php
+++
@@ -1,8 +0,0 @@
-<?php
-// debug_post.php
-if (file_exists('debug_post.txt')) {
-    echo nl2br(htmlspecialchars(file_get_contents('debug_post.txt')));
-} else {
-    echo '沒有 debug_post.txt';
-}
-?>
