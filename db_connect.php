91de2d6902c62c09a3999858bae3bf4a81e1fe29
BB <mobbsong@gmail.com>
2025-09-15 08:31:44 +0800

    將資料庫連線資訊移至 .env 檔案，並新增 .gitignore 以忽略 .env 檔案

--- db_connect.php
+++ db_connect.php
@@ -1,9 +1,16 @@
 <?php
 // db_connect.php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "food_forum";
+
+// 載入 .env 檔案
+if (file_exists(__DIR__ . '/.env')) {
+    $env = parse_ini_file(__DIR__ . '/.env');
+    $servername = $env['DB_SERVER'] ?? '';
+    $username = $env['DB_USERNAME'] ?? '';
+    $password = $env['DB_PASSWORD'] ?? '';
+    $dbname = $env['DB_NAME'] ?? '';
+} else {
+    die("找不到 .env 檔案");
+}
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
